2db4e97c8c0d0c11f0a7c301f28bcbc803485e47
Bendi2002 <90409672+Bendi2002@users.noreply.github.com>
2021-09-12 21:27:13 +0200

    Add files via upload

--- dice.cpp
+++ dice.cpp
@@ -5,17 +5,72 @@ int main() {
 	printf("\033c");
 
 	double money = 100;
+	double money1;
 	double player_bet;
-	int dice;
+	int dice = 5;
 	int dice_bet;
 	int ending = 1;
 	int round = 1;
+	int difficulty;
+	double diff_mult;
+	int lost;
+	int game_mode;
+
+	cout << "Dice betting game (really simple)\nPlease choose a difficulty:\nEasy (1)\nNormal (2)\nHard (3)\nExtreme (4)\n";
+	while (1 > difficulty || difficulty > 4) {
+
+		cin >> difficulty;
 	
-	cout << "Dice betting game (really simple)\n\n";
-	
+	}
+
+	printf("\033c");
+
+	cout << "Choose a game mode:\nEndless (1)\nBlind tournament (2)\n";
+
+	while (1 > game_mode || game_mode > 2) {
+
+		cin >> game_mode;
+
+	}
+
+	if (difficulty == 1) {
+		diff_mult = 0.5;
+	}
+
+	else if (difficulty == 2) {
+		diff_mult = 1;
+	}
+
+	else if (difficulty == 3) {
+		diff_mult = 3;
+	}
+
+	else if (difficulty == 4) {
+		diff_mult = 10;
+	}
+
 	while (money > 0 && ending != 0) {
 
-		cout << "Round " << round << "\n";
+		printf("\033c");
+
+		cout << "Round " << round << "\n\n";
+		cout << "Difficulty: ";
+
+		if(difficulty == 1) {
+			cout << "Easy\nLoss multiplier: " << diff_mult << "\n";
+		}
+
+		else if(difficulty == 2) {
+			cout << "Normal\nLoss multiplier: " << diff_mult << "\n";
+		}
+
+		else if(difficulty == 3) {
+			cout << "Hard\nLoss multiplier: " << diff_mult << "\n";
+		}
+
+		else if(difficulty == 4) {
+			cout << "Extreme\nLoss multiplier: " << diff_mult << "\n";
+		}
 
 		cout << "You currently have $" << money << "\n";
 		cout << "What do you think the dice is going to roll?\n";
@@ -30,25 +85,35 @@ int main() {
 
 			cin >> player_bet;
 
-		} while (0 > player_bet || player_bet > money);
+		} while (1 > player_bet || player_bet > money);
 
 		cout << "\n" << "Rolling the dice...\n";
-		dice = randint(5) + 1;
+		srand((unsigned) time(0));
+		int dice = 1 + (rand() % 6);
 		cout << "The dice rolls to " << dice << "\n";
 
 		if (dice == dice_bet) {
 
 			money = money + player_bet;
-			cout << "YOU WIN!!!\nNow you have $" << money << "!\n";
+			cout << "YOU WIN!!!\n";
+			cout << "You won $" << player_bet <<"!\n";
+			cout << "Now you have $" << money << "!\n";
 
 		}
 		else {
+		
+			money = money - player_bet * diff_mult;
 
-			money = money - player_bet;
-			cout << "You lost!\nNow you have $" << money << "!\n";
+			if (money < 0) {
+				money = 0;
+			}
+
+			cout << "Bad guess!\n";
+			cout << "You lost $" << player_bet * diff_mult << "!\n";
+			cout << "Now you have $" << money << "!\n";
 
 		}
-		if (money != 0) {
+		if (money != 0 && game_mode == 1) {
 
 		do {
 			cout << "Please press (1) for a new round or press (0) to exit: \n";
@@ -60,9 +125,33 @@ int main() {
 		round = round + 1;
 		
 		}
+
+		else if (money != 0 && game_mode == 2) {
+
+			if (round == 10) {
+
+				cout << "Game over!\n";
+				cout << "You have $" << money << " after 10 rounds!\n";
+				break; 
+
+			}
+			else cout << "Next round...\n";
+			round = round + 1;
+
+		}
+
 		else {
 
-			cout << "Game over!\nYou lasted " << round << " round(s)!\n";
+			cout << "Game over!\n";
+			if (round == 1) {
+
+				cout << "You lasted only 1 round!\n";
+			
+			}
+			else cout << "You lasted " << round << " rounds!\n";
 		}
 	}
+
+	return 0;
+
 }
